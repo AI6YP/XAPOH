f1c2886950e4b7c3ea09702e479bd09467798bd2
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2025-06-12 15:14:35 +0300

    LED fix

--- docs/_import/components/esp-install.a9118c8a.js
+++ docs/_import/components/esp-install.a9118c8a.js
@@ -2,7 +2,7 @@
 
 import { md5 } from "../../_npm/js-md5@0.8.3/2eae20c2.js";
 import { ESPLoader, Transport } from "../../_npm/esptool-js@0.5.5/bbcc8c77.js";
-import { bins } from "./bins.86b032e3.js";
+import { bins } from "./bins.9528a007.js";
 import { t2pt } from "./t2pt.630cedce.js";
 import { Terminal } from "../../_npm/@xterm/xterm@5.5.0/77fabc1a.js";
 import { FitAddon } from "../../_npm/@xterm/addon-fit@0.10.0/1861e560.js";
